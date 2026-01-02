26270c00ad082e963b5b315c68c783e729de5b06
Lamiabrc <sabullelam@gmail.com>
2026-01-02 16:30:54 +0100

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,5 @@
-// supabase/functions/_shared/cors.ts
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://export-navigator-orli.vercel.app",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
