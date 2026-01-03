eb18675fc79424d4a1a6bab7ed28b7c32ff2a4c4
Lamia BRECHET <sabullelam@gmail.com>
2026-01-03 21:35:00 +0100

    Update WatchCommercial.tsx

--- src/pages/WatchCommercial.tsx
+++ src/pages/WatchCommercial.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { MainLayout } from "@/components/layout/MainLayout";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
