6d593ba9250f97093e8f294ae301d56572923e24
Lamia BRECHET <sabullelam@gmail.com>
2026-01-06 14:39:39 +0100

    pak
    pak

--- src/components/dashboard/SvgMapWorld.tsx
+++ src/components/dashboard/SvgMapWorld.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import svgMap from "svgmap";
 import "svgmap/dist/svgMap.css";
+import "svg-pan-zoom/dist/svg-pan-zoom.min.js"; // polyfill needed by svgmap bundling (CSS export)
 import worldMap from "@/assets/world-map.svg";
 import { TERRITORY_PCT } from "@/domain/geo/territoryPct";
 
