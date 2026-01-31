00d445ae1a11bc5dd698baf92ffd975c797c5d16
Lamiabrc <sabullelam@gmail.com>
2026-01-31 18:59:27 +0100

    Add allowCors function to handle CORS headers

--- api/_supabase.ts
+++ api/_supabase.ts
@@ -7,15 +7,15 @@ export const supabaseAdmin = createClient(url, serviceKey, {
   auth: { persistSession: false, autoRefreshToken: false },
 });
 
+export function allowCors(res: any) {
+  res.setHeader("access-control-allow-origin", "*");
+  res.setHeader("access-control-allow-methods", "GET,POST,OPTIONS");
+  res.setHeader("access-control-allow-headers", "content-type");
+}
+
 export function json(res: any, status: number, body: any) {
   res.status(status);
   res.setHeader("content-type", "application/json; charset=utf-8");
   res.setHeader("cache-control", "no-store");
   res.end(JSON.stringify(body));
-}
-
-export function allowCors(res: any) {
-  res.setHeader("access-control-allow-origin", "*");
-  res.setHeader("access-control-allow-methods", "GET,POST,OPTIONS");
-  res.setHeader("access-control-allow-headers", "content-type");
 }
